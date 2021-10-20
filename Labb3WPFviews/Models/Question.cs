6dfd9f017eaaa55deb0d3d03653bb351a6dc6ddb
SwiftBinaryCode <89089012+SwiftBinaryCode@users.noreply.github.com>
2021-10-20 14:10:47 +0200

    Add project files.

---
+++ Labb3WPFviews/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿namespace Labb3WPFviews.Models
+{
+    class Question
+    {
+        public string Statement { get; }
+        public string[] Answers { get; }
+
+        private readonly int correctAnswer;
+
+        public int GetCorrectAnswer()
+        {
+            return correctAnswer;
+        }
+    }
+}
---
+++ Labb3WPFviews/Models/Quiz.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace Labb3WPFviews.Models
+{
+    class Quiz
+    {
+        public ICollection Questions { get; }
+        public string Title { get; }
+
+
+        public Question GetRandomQuestion()
+        {
+
+        }
+        public void AddQuestion(string statement, params string[] answers, int correctAnser)
+        {
+
+        }
+        public void RemoveQuestion(int index)
+        {
+
+        }
+    }
+}
